9102cf7b20a75751f4f611a2c1749109cc6f4911
Pavlushka66 <pavlushka66@gmail.com>
2020-08-29 18:13:11 -0500

    Init c# project

---
+++ Algo/Some/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Some
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
