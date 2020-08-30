467a9095ec1e7d447eeb96555e17f6815361fe47
Pavlushka66 <pavlushka66@gmail.com>
2020-08-30 15:14:36 -0500

    Added tests project and Number Of Islands implementation and tests

---
+++ Algo/Some/NumberOfIslands/NumberOfIslandsRunner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace Some.NumberOfIslands
+{
+    public static class NumberOfIslandsRunner
+    {
+        public static int Run(int[][] input)
+        {
+            if (input == null || input.Length == 0 || input[0].Length == 0)
+                return 0;
+            var width = input.Length;
+            var height = input[0].Length;
+            var result = 0;
+            var visited = new HashSet<KeyValuePair<int, int>>();
+
+            for (var row = 0; row < width; row++)
+            {
+                for (var col = 0; col < height; col++)
+                {
+                    var current = new KeyValuePair<int, int>(row, col);
+                    if (!visited.Contains(current) && input[row][col] == 1)
+                    {
+                        result++;
+
+                        var queue = new Queue<KeyValuePair<int, int>>();
+                        queue.Enqueue(current);
+
+                        while (queue.Count != 0)
+                        {
+                            var (x, y) = queue.Dequeue();
+                            var right = new KeyValuePair<int,int>(x + 1, y);
+                            ProcessCell(right, input, visited, queue, true);
+                            var bottom = new KeyValuePair<int,int>(x, y + 1);
+                            ProcessCell(bottom, input, visited, queue, true);
+                            var left = new KeyValuePair<int,int>(x - 1, y);
+                            ProcessCell(left, input, visited, queue, false);
+                            var top = new KeyValuePair<int,int>(x, y - 1);
+                            ProcessCell(top, input, visited, queue, false);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static void ProcessCell(KeyValuePair<int, int> cellToCheck, int[][] input,
+            ISet<KeyValuePair<int, int>> visited, Queue<KeyValuePair<int, int>> queue, bool caNtBeVisited)
+        {
+            if (cellToCheck.Key >= 0 && cellToCheck.Key < input.Length &&
+                cellToCheck.Value >= 0 && cellToCheck.Value < input[0].Length &&
+                input[cellToCheck.Key][cellToCheck.Value] == 1 && // take value from array is faster than hash calculation
+                (caNtBeVisited || !visited.Contains(cellToCheck)))
+            {
+                visited.Add(cellToCheck);
+                queue.Enqueue(cellToCheck);
+            }
+        }
+    }
+}
---
+++ Algo/SomeTests/NumberOfIslands/NumberOfIslandsTest.cs
@@ -0,0 +1,112 @@
+using NUnit.Framework;
+using Some.NumberOfIslands;
+
+namespace SomeTests.NumberOfIslands
+{
+    public class NumberOfIslandsTest
+    {
+        [Test]
+        public void NullInput()
+        {
+            int[][] input = null;
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void ZeroSizeInput()
+        {
+            var input = new int[0][];
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Size1X0Input()
+        {
+            var input = new[]
+            {
+                new int[0]
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void ThreeIslands()
+        {
+            var input = new[]
+            {
+                new [] {1,1,0,0,0,0,0},
+                new [] {1,1,0,0,0,0,0},
+                new [] {0,0,1,0,0,0,0},
+                new [] {0,0,0,1,1,1,1},
+                new [] {0,0,0,1,1,1,1},
+                new [] {0,0,0,1,1,1,1}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(3, result);
+        }
+        
+        [Test]
+        public void OneIsland()
+        {
+            var input = new[]
+            {
+                new [] {1,1,1},
+                new [] {1,1,1},
+                new [] {1,1,1}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(1, result);
+        }
+        
+        [Test]
+        public void OneIslandTurnRight()
+        {
+            var input = new[]
+            {
+                new [] {1,0},
+                new [] {1,1}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(1, result);
+        }
+        
+        [Test]
+        public void OneIslandTurnUp()
+        {
+            var input = new[]
+            {
+                new [] {0,1},
+                new [] {1,1}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(1, result);
+        }
+        
+        [Test]
+        public void OneIslandTurnLeft()
+        {
+            var input = new[]
+            {
+                new [] {1,1},
+                new [] {0,1}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(1, result);
+        }
+        
+        [Test]
+        public void OneIslandTurnDown()
+        {
+            var input = new[]
+            {
+                new [] {1,1},
+                new [] {1,0}
+            };
+            var result = NumberOfIslandsRunner.Run(input);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
