3f43e1baf1b883f3c0ecaa6b8e8cdbcc3461f981
Pavlushka66 <pavlushka66@gmail.com>
2020-08-30 21:12:46 -0500

    LeetCode Valid Parenthesis String

---
+++ Algo/Some/ValidParenthesisString/ValidParenthesisStringRunner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Some.ValidParenthesisString
+{
+    public static class ValidParenthesisStringRunner
+    {
+        public static bool Run(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return true;
+
+            var high = 0;
+            var low = 0;
+
+            foreach (var symbol in input)
+            {
+                switch (symbol)
+                {
+                    case '(':
+                        low++;
+                        high++;
+                        break;
+                    case ')':
+                        if (high == 0) 
+                            return false;
+                        if (low > 0)
+                            low--;
+                        high--;
+                        break;
+                    case '*':
+                        if (low > 0) 
+                            low--;
+                        high++;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(symbol), symbol, $"unknown symbol [{symbol}]");
+                }
+            }
+
+            return low == 0;
+        }
+    }
+}
---
+++ Algo/SomeTests/ValidParenthesisString/ValidParenthesisStringTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using Some.ValidParenthesisString;
+
+namespace SomeTests.ValidParenthesisString
+{
+    public class ValidParenthesisStringTest
+    {
+        [Test]
+        public void NullInput()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run(null));
+        }
+        
+        [Test]
+        public void EmptyInput()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run(string.Empty));
+        }
+        
+        [Test]
+        public void OneOpenOneClose()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("()"));
+        }
+        
+        [Test]
+        public void OneOpenOneAsteriskOneClose()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("(*)"));
+        }
+        
+        [Test]
+        public void OneAsteriskOneClose()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("*)"));
+        }
+        
+        [Test]
+        public void OneOpenOneAsterisk()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("(*"));
+        }
+        
+        [Test]
+        public void OneOpen()
+        {
+            Assert.False(ValidParenthesisStringRunner.Run("("));
+        }
+        
+        [Test]
+        public void OneClose()
+        {
+            Assert.False(ValidParenthesisStringRunner.Run(")"));
+        }
+        
+        [Test]
+        public void OneAsterisk()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("*"));
+        }
+        
+        [Test]
+        public void OneCloseOneAsterisk()
+        {
+            Assert.False(ValidParenthesisStringRunner.Run(")*"));
+        }
+        
+        [Test]
+        public void OpenOpenAsteriskCloseAsterisk()
+        {
+            Assert.True(ValidParenthesisStringRunner.Run("((*)*"));
+        }
+    }
+}
