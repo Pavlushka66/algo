6e440a87da189b9f1354b6793d059d7ee36acb7e
Pavel Bazhin <pavlushka66@gmail.com>
2022-01-03 21:13:19 -0600

    .net6 Maximum Length of a Concatenated String with Unique Characters (https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/)

---
+++ Algo/Some/MaxConcatStringWUniqChar.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace Some
+{
+    public class MaxConcatStringWUniqChar
+    {
+        private class ElementData
+        {
+            public HashSet<char> Chars { get; set; }
+            public int Lenght { get; set; }
+        }
+
+        public static int Run(string[] input)
+        {
+            var data = new ElementData[input.Length];
+            var connections = new Dictionary<int, HashSet<int>>();
+            var max = 0;
+            for (var i=0; i<input.Length; ++i)
+            {
+                connections.Add(i, new HashSet<int>());
+                var currentStr = input[i];
+                data[i] = new ElementData
+                {
+                    Lenght = currentStr.Length,
+                    Chars = new HashSet<char>()
+                };
+                max = currentStr.Length > max ? currentStr.Length : max;
+                var currentData = data[i];
+                foreach (var chr in currentStr)
+                {
+                    currentData.Chars.Add(chr);
+                    for (var p = 0; p < i; ++p)
+                    {
+                        var pChars = data[p].Chars;
+                        if (pChars.Contains(chr))
+                        {
+                            connections[i].Add(p);
+                            connections[p].Add(i);
+                        }
+                    }
+                }
+            }
+
+            for (var i = 0; i < data.Length; ++i)
+            {
+                var currentLength = data[i].Lenght;
+                var skip = connections[i];
+                for (var j = 0; j < data.Length; j++)
+                {
+                    if (i == j || skip.Contains(j))
+                        continue;
+                    foreach (var skipElement in connections[j])
+                    {
+                        skip.Add(skipElement);
+                    }
+
+                    currentLength += data[j].Lenght;
+                }
+
+                max = currentLength > max ? currentLength : max;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Algo/SomeTests/MaxConcatStringWUniqCharTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Some;
+
+namespace SomeTests
+{
+    [TestFixture]
+    public class MaxConcatStringWUniqCharTest
+    {
+        [Test]
+        public void BasicScenario()
+        {
+            var input = new[] { "aaa", "bbb", "abc", "ccc"};
+            var result = MaxConcatStringWUniqChar.Run(input);
+            Assert.That(result, Is.EqualTo(9));
+        }
+        
+        [Test]
+        public void NoConcat()
+        {
+            var input = new[] { "aaa", "aaaa", "aaaaa", "aaaaaa"};
+            var result = MaxConcatStringWUniqChar.Run(input);
+            Assert.That(result, Is.EqualTo(6));
+        }
+        
+        [Test]
+        public void Single()
+        {
+            var input = new[] { "aaa"};
+            var result = MaxConcatStringWUniqChar.Run(input);
+            Assert.That(result, Is.EqualTo(3));
+        }
+        
+        [Test]
+        public void BasicScenario2()
+        {
+            var input = new[] { "aaa", "bbb", "abc", "ccc", "abddddddddd"};
+            var result = MaxConcatStringWUniqChar.Run(input);
+            Assert.That(result, Is.EqualTo(14));
+        }
+    }
+}
