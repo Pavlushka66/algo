135b406cc90047a4c1f21a1ab1e6ad2a5bd4e8b0
Pavel Bazhin <pavlushka66@gmail.com>
2022-01-05 21:12:55 -0600

    Partition array into N subsets with balanced sum https://leetcode.com/discuss/interview-question/430981/

---
+++ Algo/Some/PartitionArrayIntoNSubsetsWBalancedSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Some;
+
+public static class PartitionArrayIntoNSubsetsWBalancedSum
+{
+    public static List<int>[] Run(int[] input, int n)
+    {
+        if (n == 0)
+            return Array.Empty<List<int>>();
+        if (n == 1)
+            return new[]
+            {
+                new List<int>(input)
+            };
+        Array.Sort(input);
+        var sums = new long[n];
+        var result = new List<int>[n];
+        for (var i = input.Length-1; i >= 0; --i)
+        {
+            var current = input[i];
+            var addToIndex = MinMaxArrayMemeber(sums, current);
+            if (result[addToIndex] == null)
+                result[addToIndex] = new List<int>();
+
+            result[addToIndex].Add(current);
+            sums[addToIndex] += current;
+        }
+
+        return result;
+    }
+
+    private static int MinMaxArrayMemeber(long[] sums, int current)
+    {
+        if (current == 0) 
+            return 0;
+        var min = 0;
+        var findMin = current > 0;
+        for (var i = 1; i < sums.Length; i++)
+        {
+            if (findMin && sums[i] < sums[min] || sums[i] > sums[min])
+                min = i;
+        }
+        
+        return min;
+    }
+}
---
+++ Algo/SomeTests/PartitionArrayIntoNSubsetsWBalancedSumTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Some;
+
+namespace SomeTests;
+
+[TestFixture]
+public class PartitionArrayIntoNSubsetsWBalancedSumTests
+{
+    [Test]
+    public void ZeroBuckets()
+    {
+        var input = new [] {1,1,1};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 0);
+        
+        Assert.That(result.Length, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void OneBucket()
+    {
+        var input = new [] {1,1,1};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 1);
+        
+        var actual = result.Select(a => a?.ToArray() ?? new int[0]).ToArray();
+        Assert.That(actual.Length, Is.EqualTo(1));
+        Assert.That(actual[0], Is.EquivalentTo(new [] {1, 1, 1}));
+    }
+    
+    [Test]
+    public void BasicScenario()
+    {
+        var input = new [] {1,1,1,1,1,1,1000};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 4);
+
+        var actual = result.Select(a => a?.ToArray() ?? new int[0]).ToArray();
+        Assert.That(actual.Length, Is.EqualTo(4));
+        Assert.That(actual, Is.EquivalentTo(new [] {new [] {1000}, new [] {1,1}, new [] {1,1}, new [] {1,1}}));
+    }
+    
+    [Test]
+    public void AlmostEqual()
+    {
+        var input = new [] {1,1,1,1,1,1,2,1};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 4);
+
+        var actual = result.Select(a => a?.ToArray() ?? new int[0]).ToArray();
+        Assert.That(actual.Length, Is.EqualTo(4));
+        Assert.That(actual, Is.EquivalentTo(new [] {new [] {2, 1}, new [] {1,1}, new [] {1,1}, new [] {1,1}}));
+    }
+    
+    [Test]
+    public void ZeroElementStillPresentInResult()
+    {
+        var input = new [] {1,1,1,1,0};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 4);
+
+        var actual = result.Select(a => a?.ToArray() ?? new int[0]).ToArray();
+        Assert.That(actual.Length, Is.EqualTo(4));
+        Assert.That(actual, Is.EquivalentTo(new [] {new [] {1,0}, new [] {1}, new [] {1}, new [] {1}}));
+    }
+    
+    [Test]
+    public void NegativeElement()
+    {
+        var input = new [] {1,1,1,2,-1};
+        var result = PartitionArrayIntoNSubsetsWBalancedSum.Run(input, 4);
+
+        var actual = result.Select(a => a?.ToArray() ?? new int[0]).ToArray();
+        Assert.That(actual.Length, Is.EqualTo(4));
+        Assert.That(actual, Is.EquivalentTo(new [] {new [] {2,-1}, new [] {1}, new [] {1}, new [] {1}}));
+    }
+}
