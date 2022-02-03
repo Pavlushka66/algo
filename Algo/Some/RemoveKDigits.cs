bf2a94e3d3613a35246e244ee811fd8f4b6373eb
Pavel Bazhin <pavlushka66@gmail.com>
2022-02-02 19:36:43 -0600

    Remove k digits first approach https://leetcode.com/problems/remove-k-digits/

---
+++ Algo/Some/RemoveKDigits.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Some;
+
+public static class RemoveKDigits
+{
+    public static long Run(string input, int k)
+    {
+        var numLen = input.Length;
+        if (numLen <= k)
+            return 0;
+
+        if (k < 1)
+            return long.Parse(input);
+
+        var result = new StringBuilder("0");
+        var i = 1;
+        var previous = int.Parse(input[0].ToString());
+        while (i < numLen)
+        {
+            var current = int.Parse(input[i].ToString());
+            var charsLeft = numLen - (i + 1);
+            
+            Console.WriteLine($"current({current}) previous({previous}) k({k}) charsLeft({charsLeft}) result({result})");
+
+            if (charsLeft == 0)
+            {
+                if (k == 1)
+                {
+                    result.Append(Math.Min(current, previous));
+                }
+                if (k == 0)
+                {
+                    result.Append(previous);
+                    result.Append(current);
+                }
+            }
+            else
+            {
+                if ((k == 0 || current > previous) && k <= charsLeft+1) {
+                    result.Append(previous);
+                }
+                else
+                {
+                    --k;
+                }
+            }
+
+            previous = current;
+            ++i;
+        }
+        
+        return long.Parse(result.ToString());
+    }
+}
---
+++ Algo/SomeTests/RemoveKDigitsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Some;
+
+namespace SomeTests;
+
+[TestFixture]
+public class RemoveKDigitsTest
+{
+    [TestCase("101", 2, 0L)]
+    [TestCase("321", 2, 1L)]
+    [TestCase("1", 2, 0L)]
+    [TestCase("1", 1, 0L)]
+    [TestCase("1", 0, 1L)]
+    [TestCase("123456", 5, 1L)]
+    [TestCase("11111111", 5, 111L)]
+    [TestCase("11111123", 5, 111L)]
+    public void Simple(string input, int k, long expected)
+    {
+        var actual = RemoveKDigits.Run(input, k);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
